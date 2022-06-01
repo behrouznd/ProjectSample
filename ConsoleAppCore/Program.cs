88733ea598fe97c3fdcd685796a0eba272a30ded
behrouz naderi <behrouz1986@gmail.com>
2022-06-01 13:29:19 +0430

    ChangeTracker.TrackGraph

--- ConsoleAppCore/Program.cs
+++ ConsoleAppCore/Program.cs
@@ -7,14 +7,31 @@ namespace ConsoleAppCore
     {
         static void Main(string[] args)
         {
-            //--Access Shadow Property--
+            
             using (var context   = new SchoolContext())
             {
+                //--Access Shadow Property--
                 var std = new Student() { Name = "Bill" };
                 context.Entry(std).Property("CreatedDate").CurrentValue = DateTime.Now;
                 var createdTime = context.Entry(std).Property("CreatedDate").CurrentValue;
+                //----   
+
+
+                //--ChangeTracker.TrackGraph()--
+                context.ChangeTracker.TrackGraph(std, e =>
+               {
+                   if (e.Entry.IsKeySet)
+                   {
+                       e.Entry.State = Microsoft.EntityFrameworkCore.EntityState.Unchanged;
+                   }
+                   else
+                   {
+                       e.Entry.State = Microsoft.EntityFrameworkCore.EntityState.Added;
+                   }
+               });
+                //----
             }
-            //----    
+             
 
 
             Console.WriteLine("Hello World!");
